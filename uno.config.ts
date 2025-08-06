f8c8b7f09419a243fddf6f385ff4655dd0a9bf45
鲨鱼辣椒 <monishine_code@163.com>
2025-08-06 16:38:48 +0800

    refactor: 配置unocss

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,20 @@
-import { defineConfig } from 'unocss'
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetWind3,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
 
 export default defineConfig({
-  // ...UnoCSS options
+  presets: [
+    presetWind3(),
+    presetIcons(),
+    presetAttributify(),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
 })
